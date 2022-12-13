aa465bda3ba8b8c1509cf2629ee16c3a640e99f4
mayuino <mendo.woker@gmail.com>
2022-12-14 00:33:57 +0900

    ✨ 社内貢献ポイントページ追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {Noto_Sans_JP} from '@next/font/google'
+
+const noto_sans_jp = Noto_Sans_JP({
+  subsets:['japanese'],
+  weight:['500'],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={noto_sans_jp.className}>
+    <Component {...pageProps} />
+   </main>
+   );
 }
